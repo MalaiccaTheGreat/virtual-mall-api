8025a15a71ed608e3d32bd9ad5532dce3c7b6e12
Joseph Malaicca <josekayzora.jk@gmail.com>
2025-08-20 11:44:55 +0200

    Add product_features and product_images migrations

--- database/migrations/2024_03_21_000006_create_product_features_table.php
+++ database/migrations/2024_03_21_000006_create_product_features_table.php
@@ -1,1 +1,22 @@
- 
+ <?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductFeaturesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('product_features', function (Blueprint $table) {
+            $table->id();
+            $table->string('feature_name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('product_features');
+    }
+}
